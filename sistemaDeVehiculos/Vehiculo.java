a13aac607d1ff923fd15c570a061cd6d9b7c32cf
Mauro Presso <mauro.presso@gmail.com>
2026-04-30 14:29:29 -0300

    Ya esta codificada la clase padre  segun lo que pide el enunciado

---
+++ sistemaDeVehiculos/Vehiculo.java
@@ -0,0 +1,29 @@
+package sistemaDeVehiculos;
+
+public abstract class Vehiculo 
+{
+    private String marca;
+    private String modelo;
+
+
+    public Vehiculo(String marca, String modelo) 
+    {
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+
+    public String getMarca() 
+    {
+        return marca;
+    }
+
+    public String getModelo() 
+    {
+        return modelo;
+    }
+
+    public void mostrarInfo() 
+    {
+        System.out.println("Marca: " + marca + ", Modelo: " + modelo);
+    }
+}

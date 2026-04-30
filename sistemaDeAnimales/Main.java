e7fa2af0bf6ef09e3a50160aad801b0827d95341
Mauro Presso <mauro.presso@gmail.com>
2026-04-30 20:03:38 -0300

    Optimize el main porque no hacia falta validar

--- sistemaDeAnimales/Main.java
+++ sistemaDeAnimales/Main.java
@@ -11,24 +11,9 @@ public class Main {
 
         for (Animal animal : animales) 
         {
-            if (animal instanceof Perro) 
-            {
-                Perro perro = (Perro) animal;
-                System.out.println("El perro se llama " + perro.getNombre() + " , tiene " + perro.getEdad() + " años, su raza es " + perro.getRaza() + "y hace el sonido: ");
-                perro.hacerSonido();
-            } 
-            else if (animal instanceof Gato) 
-            {
-                Gato gato = (Gato) animal;
-                System.out.println("El gato se llama " + gato.getNombre() + " , tiene " + gato.getEdad() + " años, su raza es " + gato.getRaza() + "y hace el sonido: ");
-                gato.hacerSonido();
-            } 
-            else if (animal instanceof Pajaro) 
-            {
-                Pajaro pajaro = (Pajaro) animal;
-                System.out.println("El pájaro se llama " + pajaro.getNombre() + " y tiene " + pajaro.getEdad() + " años. Su tipo es " + pajaro.getTipo() + " y hace el sonido: ");
-                pajaro.hacerSonido();
-            }
+            System.out.println();
+            System.out.println("El animal se llama " + animal.getNombre() + " y tiene " + animal.getEdad() + " años. Hace el sonido: ");
+            animal.hacerSonido();
         }
     }
 }

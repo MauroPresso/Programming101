e745a9150af0fc5372baf80425de453ec42df279
Mauro Presso <mauro.presso@gmail.com>
2026-05-19 19:53:21 -0300

    Enumerador Estado Libro.

---
+++ sistemaDeBiblioteca/EstadoLibro.java
@@ -0,0 +1,12 @@
+package sistemaDeBiblioteca;
+
+/*!
+ * @brief Enum que representa el estado actual de un libro dentro del sistema de biblioteca.
+ * 
+ *        Un libro puede estar disponible para ser prestado o puede encontrarse actualmente prestado.
+ */
+public enum EstadoLibro 
+{
+    DISPONIBLE,
+    PRESTADO
+}

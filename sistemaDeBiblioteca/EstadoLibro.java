c3db17c3c61b30a8d3b8608ac36dd9b253644788
Mauro Presso <mauro.presso@gmail.com>
2026-05-19 19:54:10 -0300

    .

--- sistemaDeBiblioteca/EstadoLibro.java
+++ sistemaDeBiblioteca/EstadoLibro.java
@@ -2,7 +2,6 @@ package sistemaDeBiblioteca;
 
 /*!
  * @brief Enum que representa el estado actual de un libro dentro del sistema de biblioteca.
- * 
  *        Un libro puede estar disponible para ser prestado o puede encontrarse actualmente prestado.
  */
 public enum EstadoLibro 
